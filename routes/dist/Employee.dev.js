5b78d5147a9442f805c6b093cd5ad03d88b3459e
Lakshmi Sowjanya Kalyanam <s550015@nwmissouri.edu>
2022-11-30 20:42:37 -0600

    Updated Employee.js

--- routes/dist/Employee.dev.js
+++ routes/dist/Employee.dev.js
@@ -2,36 +2,30 @@
 
 var express = require('express');
 
-var router = express.Router();
-/* GET home page. */
-
-router.get('/', function (req, res, next) {
-  res.render('Employee', {
-    title: 'Search Results Employee'
-  });
-});
-
-var express = require('express');
-
 var Employee_controlers = require('../controllers/Employee');
 
 var router = express.Router();
-/* GET Employee */
+
+var secured = function secured(req, res, next) {
+  if (req.user) {
+    return next();
+  }
+
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+};
 
 router.get('/', Employee_controlers.Employee_view_all_Page);
-module.exports = router; // GET request for one Employee.
-
 router.get('/Employee/:id', Employee_controlers.Employee_detail);
-/* GET detail Employee page */
-
-router.get('/detail', Employee_controlers.Employee_view_one_Page);
+router.get('/detail', secured, Employee_controlers.Employee_view_one_Page);
 /* GET create Employee page */
 
-router.get('/create', Employee_controlers.Employee_create_Page);
-/* GET create update page */
+router.get('/create', secured, Employee_controlers.Employee_create_Page);
+/* GET create Employee page */
 
-router.get('/update', Employee_controlers.Employee_update_Page);
+router.get('/update', secured, Employee_controlers.Employee_update_Page);
 /* GET delete Employee page */
 
-router.get('/delete', Employee_controlers.Employee_delete_Page);
+router.get('/delete', secured, Employee_controlers.Employee_delete_Page);
+module.exports = router;
 //# sourceMappingURL=Employee.dev.js.map
