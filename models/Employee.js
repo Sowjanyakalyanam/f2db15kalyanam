8baa1eec0c3cc92787dc44b22c8a2c68eb2d9124
Lakshmi Sowjanya Kalyanam <s550015@nwmissouri.edu>
2022-11-30 20:49:55 -0600

    Updated Employee.js

--- models/Employee.js
+++ models/Employee.js
@@ -1,7 +1,17 @@
 const mongoose = require("mongoose")
 const EmployeeSchema = mongoose.Schema({
-EmpName: String,
-EmpType: String,
-EmpSal: Number,
+EmpName: {
+    type: String, 
+    required: true
+},
+EmpType: {
+    type: String, 
+    required: true
+},
+EmpSal: {
+    type: Number, 
+    min:500, max:10000,
+    required: true
+},
 })
 module.exports = mongoose.model("Employee",EmployeeSchema)
--- models/dist/Employee.dev.js
+++ models/dist/Employee.dev.js
@@ -3,9 +3,20 @@
 var mongoose = require("mongoose");
 
 var EmployeeSchema = mongoose.Schema({
-  EmpName: String,
-  EmpType: String,
-  EmpSal: Number
+  EmpName: {
+    type: String,
+    required: true
+  },
+  EmpType: {
+    type: String,
+    required: true
+  },
+  EmpSal: {
+    type: Number,
+    min: 500,
+    max: 10000,
+    required: true
+  }
 });
 module.exports = mongoose.model("Employee", EmployeeSchema);
 //# sourceMappingURL=Employee.dev.js.map
