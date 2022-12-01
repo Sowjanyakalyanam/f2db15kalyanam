f7f86bbbcc79bde8ae3fee0329a9cee3e9f4cd66
Lakshmi Sowjanya Kalyanam <s550015@nwmissouri.edu>
2022-11-30 21:10:40 -0600

    change2

--- dist/app.dev.js
+++ dist/app.dev.js
@@ -14,14 +14,6 @@ var passport = require('passport');
 
 var LocalStrategy = require('passport-local').Strategy;
 
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {}; // render the error page
-
-  res.status(err.status || 500);
-  res.render('error');
-});
 passport.use(new LocalStrategy(function (username, password, done) {
   Account.findOne({
     username: username
